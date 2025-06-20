0e33aa1b9012fce41f940cd9c690141905eaf4bc
Felipe <felipefesilva19@gmail.com>
2025-06-20 14:30:57 -0300

    Atualização aula 18

---
+++ src/components/Heading.tsx
@@ -0,0 +1,15 @@
+import styles from './Heading.module.css'
+
+type HeadingProps = {
+    children: React.ReactNode; // aceita qualquer tipo 
+}
+
+export function Heading ({children}: HeadingProps){
+    console.log(children)
+     return(
+
+      <h1 className={styles.heading}> {children} </h1>
+
+     )
+
+}
